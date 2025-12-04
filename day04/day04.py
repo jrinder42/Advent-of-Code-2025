0ca50393060b03d380e592f27682fb522c16b0a3
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-12-04 00:12:02 -0500

    day 4 part 1

---
+++ day04/day04.py
@@ -0,0 +1,54 @@
+"""
+
+Advent of Code 2025 - Day 4
+
+"""
+
+from ast import literal_eval
+
+import numpy as np
+
+
+arr = []
+with open("day04.txt", "r") as file:
+    for line in file:
+        line = line.strip('\n')
+        arr.append([char for char in line])
+
+arr = np.array(arr)
+
+# Part 1
+
+def spots(point, grid):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            r, c = point[0] + i, point[1] + j
+            # outside the grid
+            if r < 0 or c < 0 or r >= grid.shape[0] or c >= grid.shape[1]:
+                continue
+
+            if (r, c) == point:
+                continue
+
+            yield grid[r, c], (r, c)
+
+rolls_of_paper = 0
+for i in range(arr.shape[0]):
+    for j in range(arr.shape[1]):
+        val = arr[i, j]
+        if val != "@":
+            continue
+
+        count = 0
+        for spot_val, (r, c) in spots((i, j), arr):
+            if spot_val == "@":
+                count += 1
+
+        if count < 4:
+            rolls_of_paper += 1
+
+print(f'Advent of Code Day 4 Answer Part 1: {rolls_of_paper}')
+
+# Part 2
+
+print(f'Advent of Code Day 4 Answer Part 2: {1}')
