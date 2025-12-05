fbb9c769ef70542d89c8d484f0959a190c865736
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-12-05 01:04:27 -0500

    add day 5 part 2

--- day05/day05.py
+++ day05/day05.py
@@ -33,4 +33,42 @@ print(f'Advent of Code Day 5 Answer Part 1: {count}')
 
 # Part 2
 
-print(f'Advent of Code Day 5 Answer Part 2: {1}')
+all_fresh = []
+for fresh_id_range in fresh_id_ranges:
+    left, right = fresh_id_range.split("-")
+    left, right = literal_eval(left), literal_eval(right)
+    if all_fresh:
+        count = 0
+        for idx, elem in enumerate(all_fresh):
+            if elem[0] <= left <= elem[1]:
+                elem[1] = max(elem[1], right)
+                count += 1
+            elif elem[0] <= right <= elem[1]:
+                elem[0] = min(elem[0], left)
+                count += 1
+        if count == 0:  # mutually exclusive ranges
+            all_fresh.append([left, right])
+    else:
+        all_fresh.append([left, right])  # inclusive ranges as the question states
+
+# final step to merge any overlapping ranges
+all_fresh = sorted(all_fresh, key=lambda x: x[0])
+
+final_fresh = [all_fresh[0]]
+for elem in all_fresh[1:]:
+    left, right = final_fresh[-1]
+    if elem[0] <= left <= elem[1] and elem[0] <= right <= elem[1]:  # fully contained
+        continue
+
+    if left <= elem[0] <= right:
+        final_fresh[-1][1] = max(right, elem[1])
+    elif left <= elem[1] <= right:
+        final_fresh[-1][1] = min(elem[1], right)
+    else:  # mutually exclusive
+        final_fresh.append(elem)
+
+fresh_ingredients = 0
+for left, right in final_fresh:
+    fresh_ingredients += (right - left + 1)
+
+print(f'Advent of Code Day 5 Answer Part 2: {fresh_ingredients}')
