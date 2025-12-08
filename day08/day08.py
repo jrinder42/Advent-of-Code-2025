cc8c121e49a2061529ba8e6218b2ff040c5da463
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-12-08 12:48:37 -0500

    add day 8 part 2

--- day08/day08.py
+++ day08/day08.py
@@ -39,6 +39,7 @@ for idx, (key, value) in enumerate(boxes.items()):
     if idx == 1000-1:  # make this 10-1 for the test input, 1000-1 for the real input
         break
 
+# merge circuits
 for key, value in circuits.items():
     for val in value:
         diff = value - set(val)
@@ -72,4 +73,45 @@ print(f'Advent of Code Day 8 Answer Part 1: {math.prod(prod)}')
 
 # Part 2
 
-print(f'Advent of Code Day 8 Answer Part 2: {1}')
+last_junction = None
+circuits = []
+for idx, (key, value) in enumerate(boxes.items()):
+    left, right = key
+    enter = False
+    for iidx, circuit in enumerate(circuits):
+        if left in circuit:
+            enter = True
+            circuits[iidx].add(right)
+        elif right in circuit:
+            enter = True
+            circuits[iidx].add(left)
+
+    # merge circuits if needed
+    to_remove = set()
+    for c1, c2 in combinations(circuits, 2):
+        inter = len(c1.intersection(c2)) > 0  # same as len(c2.intersection(c1)) > 0
+        if inter:  # c1 vs c2 order does not matter
+            # merge on c1
+            c1_idx = circuits.index(c1)
+            circuits[c1_idx].update(c2)
+
+            # add c2 to removal set
+            c2_idx = circuits.index(c2)
+            to_remove.add(c2_idx)
+
+    for circuit_idx in to_remove:
+        circuits.pop(circuit_idx)
+
+    # mutually exclusive, add new circuit
+    if not enter:
+        circuits.append({left, right})
+
+    for circuit in circuits:
+        if len(circuit) == len(arr):
+            last_junction = key
+            break
+
+    if last_junction:
+        break
+
+print(f'Advent of Code Day 8 Answer Part 2: {last_junction[0][0] * last_junction[1][0]}')
